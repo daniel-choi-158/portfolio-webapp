ea518148ec1e556336b4c9109fd4382c68a62761
danielchoi158 <danielchoi158@gmail.com>
2020-06-15 10:21:01 +1000

    WEB-28 Frontend app with my custom theme

---
+++ frontend/portfolio-frontend/src/mock/data.js
@@ -0,0 +1,84 @@
+import uuidv1 from 'uuid/v1';
+
+// HEAD DATA
+export const headData = {
+  title: 'Daniel Choi | DevOps Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Daniel.',
+  subtitle: "I'm a DevOps Engineer based in Sydney.",
+  cta: 'About me',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.png',
+  paragraphOne: 'Para One',
+  paragraphTwo: 'Para Two',
+  paragraphThree: 'HMM',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: 'This Website',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'danielchoi158@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: uuidv1(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: uuidv1(),
+      name: 'github',
+      url: '',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
